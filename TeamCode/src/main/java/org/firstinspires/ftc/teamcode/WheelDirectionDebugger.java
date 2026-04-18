debf3d4416e194dc4e25e0cfe9af8d9ec3ecdf47
JoelyBoi <joel.a.holder@outlook.com>
2026-04-18 00:23:46 -0500

    Added panels and heading tuner

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WheelDirectionDebugger.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class WheelDirectionDebugger extends OpMode {
+    private DcMotor fr, br, fl, bl;
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
