8ecd00e6eac3de237e10008bca4259fe6c476323
Xhaemel <xhaemel@gmail.com>
2026-04-23 07:44:10 -0700

    javadocs/small cleanup

--- TeamCode/src/main/java/drivetrains/Swerve.java
+++ TeamCode/src/main/java/drivetrains/Swerve.java
@@ -65,6 +65,15 @@ public class Swerve extends Drivetrain {
         double frPower = Math.sqrt(Math.pow(strafeFront, 2) + Math.pow(forwardRight, 2));
         double brPower = Math.sqrt(Math.pow(strafeRear, 2) + Math.pow(forwardRight, 2));
 
+        //current limiting
+        double currentRatio = getTotalCurrent()/constants.totalMaxCurrent;
+        //normalizer current
+        if(getTotalCurrent() > constants.totalMaxCurrent){
+            flPower /= currentRatio;
+            frPower /= currentRatio;
+            blPower /= currentRatio;
+            brPower /= currentRatio;
+        }
         // Normalize powers from -maxPower to maxPower if any exceed the max
         double max = Math.max(0, Math.abs(flPower));
         max = Math.max(max, Math.abs(blPower));
@@ -106,5 +115,13 @@ public class Swerve extends Drivetrain {
     public String toString() {
         return String.format(Locale.ENGLISH, "Swerve(fl=%s, bl=%s, fr=%s, br=%s)",
                 fl.toString(), bl.toString(), fr.toString(), br.toString());
+    }
+
+    /**
+     * gets the total current of the drivetrain
+     * @return the current in Amps
+     */
+    private double getTotalCurrent(){
+        return fl.getCurrent() + fr.getCurrent() + bl.getCurrent() + br.getCurrent();
     }
 }
